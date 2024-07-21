6179e10bad15acf659810541f2b727443b58a17b
Edie <urraed@hotmail.com>
2024-07-21 12:47:56 -0600

    Update deloy

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,13 +1,9 @@
 using CentroEspecialidadesDentales.Application.Common.Interfaces;
 using CentroEspecialidadesDentales.Application.Services;
-using CentroEspecialidadesDentales.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Net.Http;
 using WebUI.Components;
-using WebUI.Components.Pages;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -16,14 +12,11 @@ builder.Services.AddRazorComponents()
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddWebUIServices();
-//builder.Services.AddServerSideBlazor();
+
 builder.Services.AddAntiforgery(options => options.HeaderName = "X-CSRF-TOKEN");
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IEmailService, SmtpEmailService>();
 builder.Services.AddScoped<BookingService>();
 builder.Services.AddScoped<AppointmentService>();
-
-
 
 var app = builder.Build();
 
@@ -36,28 +29,17 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 
 app.UseHealthChecks("/health");
 
-//app.UseSwaggerUi3(settings =>
-//{
-//    settings.Path = "/api";
-//    settings.DocumentPath = "/api/specification.json";
-//});
-
 app.UseRouting();
 app.UseAntiforgery();
-
 app.UseAuthentication();
-//app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
@@ -66,7 +48,8 @@ app.MapControllerRoute(
 
 app.MapRazorPages();
 
-//app.MapFallbackToFile("index.html");
+// Ensure the Blazor WebAssembly app serves the index.html
+app.MapFallbackToFile("index.html");
 
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
